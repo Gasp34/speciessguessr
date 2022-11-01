c2a86332cce6e5f8f9dc8a23f23ff23c1bbf244a
Gaspard34 <gaspard.dussert@gmail.com>
2022-11-01 18:46:32 +0100

    feat: finish welcome menu

--- speciesguessr/gui.py
+++ speciesguessr/gui.py
@@ -10,55 +10,46 @@ from time import sleep
 
 from speciesguessr.species import SpeciesInfo
 from speciesguessr.guessr import Guessr
-from speciesguessr.utils import set_answers, get_new_guess
+from speciesguessr.utils import set_answers, get_new_guess, taxon_to_id, place_to_id, text_dict, lang_dict
 
 
 class Config():
-    def __init__(self, language):
-        self.place_id = 6753 # 162266 for mtp
-        self.taxon_id = 3
+    def __init__(self, language, place_id, taxon_id, popular):
+        self.place_id = place_id
+        self.taxon_id = taxon_id
         self.language = language
         w, h = sg.Window.get_screen_size()
         self.height = h - 200
         self.width = w - 100
         self.image_quality = "large"
-        self.popular = True
+        self.popular = popular
 
 
-text_dict = {"language": {"fr": "Langue du logiciel", "en": "Software language"},
-             "languages": {"fr": ["français", "english"], "en": ["english", "français"]},
-             "species_language": {"fr": "Langue des noms d'espèces", "en": "Species language"},
-             "species_languages": {"fr": ["français", "english", "latin"], "en": ["english", "français", "latin"]},
-             "taxon": {"fr": "Taxon", "en": "Taxon"},
-             "taxons": {"fr": ["oiseau", "reptile"], "en": ["bird", "reptile"]},
-             "place": {"fr": "Zone géographique", "en": "Geographical area"},
-             "places": {"fr": ["France", "Montpellier"], "en": ["France", "Montpellier"]}}
-
-lang_dict = {"english": "en", "français": "fr", "latin": "latin"}
-
-
-sg.theme('Reddit')
-
-def create_layout(lang):
+def menu_layout(lang):
     def text(key):
         return text_dict[key][lang]
+
     layout = [[sg.Column(justification="center", layout=[[sg.Text(text("language")),
-                           sg.Combo(text("languages"), enable_events=True, key="combo", default_value=text("languages")[0])]])],
+                           sg.Combo(text("languages"), enable_events=True, key="languages", default_value=text("languages")[0])]])],
               [sg.Column(justification="center", layout=[[sg.Text(text("species_language")),
-                           sg.Combo(text("species_languages"), key="combo_species", default_value=text("species_languages")[0])]])],
+                           sg.Combo(text("species_languages"), key="species_languages", default_value=text("species_languages")[0])]])],
               [sg.Column(justification="center", layout=[[sg.Text(text("place")),
                            sg.Combo(text("places"), key="places", default_value=text("places")[0])]])],
               [sg.Column(justification="center", layout=[[sg.Text(text("taxon")),
                            sg.Combo(text("taxons"), key="taxons", default_value=text("taxons")[0])]])],
-              [sg.Button("ok")]
+              [sg.Column(justification="center", layout=[[sg.Checkbox(text("checkbox1"), default=True, enable_events=True, key="C1"), 
+                                                          sg.Checkbox(text("checkbox2"), enable_events=True, key="C2")]])],
+              [sg.Button(text("easy"), key="easy", size=(20, 3)),
+               sg.Button(text("hard"), key="hard", size=(20, 3))]
               ]
+
     return layout
 
+sg.theme('Reddit')
 lang="fr"
-ok = False
-end = False
-while not ok and not end:
-    layout = create_layout(lang)
+mode, end = False, False
+while not mode and not end:
+    layout = menu_layout(lang)
     window = sg.Window('SpeciesGuessr', layout, finalize=True,
                        return_keyboard_events=True, font=('Helvetica', 15))
     window.TKroot.focus_force()
@@ -67,61 +58,70 @@ while not ok and not end:
         if event == sg.WIN_CLOSED or event == 'Escape:27':
             end = True
             break
-        if event == "combo":
-            lang = lang_dict[values["combo"]]
+        if event == "languages":
+            lang = lang_dict[values["languages"]]
             break
-        if event=="ok":
-            ok = True
+        if event=="easy":
+            mode = "easy"
             break
+        if event=="C1":
+            window["C2"].update(False)
+        if event=="C2":
+            window["C1"].update(False)
     window.close()
 
-if ok:
-    config = Config(language=lang_dict[values["combo_species"]])
+if mode:
+    config = Config(language=lang_dict[values["species_languages"]],
+                    place_id=place_to_id(values["places"], text_dict, lang),
+                    taxon_id=taxon_to_id(values["taxons"], text_dict, lang),
+                    popular=values["C2"])
+    print(vars(config))
 
-# species_info = SpeciesInfo(config)
-# guessr = Guessr(species_info)
-# species_to_guess, image = get_new_guess(guessr, config)
-
-# layout = [[sg.Image(key="-IMAGE-", size=(config.width, config.height))],
-#           [sg.Column([[sg.Text("Score :"), sg.Text("0/0", key="-SV-"),
-#                        sg.Text("  Accuracy :"), sg.Text("100%", key="-AV-")]],
-#                      justification="center")],
-#           [sg.B(key='A1', expand_x=True), sg.B(key='A2', expand_x=True),
-#            sg.B(key='A3', expand_x=True), sg.B(key='A4', expand_x=True)]]
-
-# sg.theme('Reddit')
-# window = sg.Window('SpeciesGuessr', layout, location=(20, 20), finalize=True,
-#                    return_keyboard_events=True, font=('Helvetica', 15))
-# window["-IMAGE-"].update(data=ImageTk.PhotoImage(image))
-# answers = set_answers(guessr, window, species_to_guess)
-# window.TKroot.focus_force()
-
-# success, fails = 0, 0
-# while True:
-#     event, values = window.read()
-#     if event == sg.WIN_CLOSED or event == 'Escape:27':
-#         break
-#     if type(event) == str and event[0]=="A":
-#         i = int(event[1])
-#         if species_to_guess == answers[i-1]:
-#             fail = False
-#             success += 1
-#             window[f"A{i}"].update(button_color="green")
-#         else:
-#             fail = True
-#             fails += 1
-#             window[f"A{i}"].update(button_color="red")
-#             j = answers.index(species_to_guess)
-#             window[f"A{j+1}"].update(button_color="green")
-#         window["-SV-"].update(f"{success}/{success+fails}")
-#         window["-AV-"].update(f"{int(success/(success+fails)*100)}%")
-#         window.refresh()
-#         if fail:
-#             sleep(1)
-
-#         species_to_guess, image = get_new_guess(guessr, config)
-#         window["-IMAGE-"].update(data=ImageTk.PhotoImage(image))
-#         answers = set_answers(guessr, window, species_to_guess)
-
-# window.close()
+if mode == "easy":
+    species_info = SpeciesInfo(config)
+    guessr = Guessr(species_info)
+    species_to_guess, image = get_new_guess(guessr, config)
+    
+    layout = [[sg.Image(key="-IMAGE-", size=(config.width, config.height))],
+              [sg.Column([[sg.Text("Score :"), sg.Text("0/0", key="-SV-"),
+                            sg.Text("  Accuracy :"), sg.Text("100%", key="-AV-")]],
+                          justification="center")],
+              [sg.B(key='A1', expand_x=True), sg.B(key='A2', expand_x=True),
+                sg.B(key='A3', expand_x=True), sg.B(key='A4', expand_x=True)]]
+    
+    sg.theme('Reddit')
+    window = sg.Window('SpeciesGuessr', layout, location=(20, 20), finalize=True,
+                        return_keyboard_events=True, font=('Helvetica', 15))
+    window["-IMAGE-"].update(data=ImageTk.PhotoImage(image))
+    answers = set_answers(guessr, window, species_to_guess)
+    window.TKroot.focus_force()
+    
+    success, fails = 0, 0
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == 'Escape:27':
+            break
+        if type(event) == str and event[0]=="A":
+            i = int(event[1])
+            if species_to_guess == answers[i-1]:
+                fail = False
+                success += 1
+                window[f"A{i}"].update(button_color="green")
+            else:
+                fail = True
+                fails += 1
+                window[f"A{i}"].update(button_color="red")
+                j = answers.index(species_to_guess)
+                window[f"A{j+1}"].update(button_color="green")
+            window["-SV-"].update(f"{success}/{success+fails}")
+            window["-AV-"].update(f"{int(success/(success+fails)*100)}%")
+            window.refresh()
+            if fail:
+                sleep(1)
+    
+            species_to_guess, image = get_new_guess(guessr, config)
+            window["-IMAGE-"].update(data=ImageTk.PhotoImage(image))
+            answers = set_answers(guessr, window, species_to_guess)
+    
+    window.close()
 
--- speciesguessr/utils.py
+++ speciesguessr/utils.py
@@ -32,3 +32,32 @@ def get_new_guess(guessr, config):
         obs = guessr.find_obs_with_photo(species_to_guess["id"])
     image = load_image(obs, config)
     return species_to_guess, image  
+
+def place_to_id(place, text_dict, lang):
+    try:
+        return int(place)
+    except ValueError:
+        place_ids = [6753, 162266]
+        return place_ids[text_dict["places"][lang].index(place)]
+
+def taxon_to_id(taxon, text_dict, lang):
+    try:
+        return int(taxon)
+    except ValueError:
+        place_ids = [3, 34]
+        return place_ids[text_dict["taxons"][lang].index(taxon)]
+
+text_dict = {"language": {"fr": "Langue du logiciel", "en": "Software language"},
+             "languages": {"fr": ["français", "english"], "en": ["english", "français"]},
+             "species_language": {"fr": "Noms d'espèces", "en": "Species names"},
+             "species_languages": {"fr": ["français", "english", "latin"], "en": ["english", "français", "latin"]},
+             "checkbox1": {"fr": "Toutes les espèces", "en": "All species"},
+             "checkbox2": {"fr": "Espèces populaires", "en": "Popular species"},
+             "easy": {"fr": "Facile", "en": "Easy"},
+             "hard": {"fr": "Difficile", "en": "Hard"},
+             "taxon": {"fr": "Taxon", "en": "Taxon"},
+             "taxons": {"fr": ["Oiseau", "Reptile"], "en": ["Bird", "Reptile"]},
+             "place": {"fr": "Zone géographique", "en": "Geographical area"},
+             "places": {"fr": ["France", "Montpellier"], "en": ["France", "Montpellier"]}}
+
+lang_dict = {"english": "en", "français": "fr", "latin": "latin"}
