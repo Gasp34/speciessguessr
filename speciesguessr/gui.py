4d20ce0e449951cfbdc528e8826c98bf9aa18932
gaspard.dussert <gaspard.dussert@creatis.insa-lyon.fr>
2022-11-08 20:44:30 +0100

    feat: add logo

--- speciesguessr/gui.py
+++ speciesguessr/gui.py
@@ -42,7 +42,7 @@ def menu_layout(lang):
               [sg.Button(text("easy"), key="easy", size=(15, 3), expand_x=True),
                sg.Button(text("medium"), key="medium", size=(15, 3), expand_x=True),
                sg.Button(text("hard"), key="hard", size=(15, 3), expand_x=True)],
-              [sg.ProgressBar(max_value=3, size=(42, 20), key="pb")]
+              [sg.ProgressBar(max_value=3, size=(1, 20), key="pb", expand_x=True)]
               ]
 
     return layout
@@ -53,7 +53,7 @@ lang = "fr"
 mode, end = False, False
 while not mode and not end:
     layout = menu_layout(lang)
-    window = sg.Window('SpeciesGuessr', layout, finalize=True,
+    window = sg.Window('SpeciesGuessr', layout, finalize=True, icon="logo.ico",
                        return_keyboard_events=True, font=('Helvetica', 15))
     window.TKroot.focus_force()
     while True:
@@ -72,85 +72,85 @@ while not mode and not end:
         if event == "C2":
             window["C1"].update(not values["C2"])
 
-if mode:
-    config = Config(language=lang_dict[values["species_languages"]],
-                    place_id=place_to_id(values["places"], text_dict, lang),
-                    taxon_id=taxon_to_id(values["taxons"], text_dict, lang),
-                    popular=values["C2"])
-    # print(vars(config))
-    end = False
-    species_info = SpeciesInfo(config)
-    window["pb"].update_bar(1)
+    if mode:
+        config = Config(language=lang_dict[values["species_languages"]],
+                        place_id=place_to_id(values["places"], text_dict, lang),
+                        taxon_id=taxon_to_id(values["taxons"], text_dict, lang),
+                        popular=values["C2"])
+        # print(vars(config))
+        end = False
+        species_info = SpeciesInfo(config)
+        window["pb"].update_bar(1)
 
-    if species_info.species_list is None:
-        end = True
-        layout = [[sg.Text({"fr": "Communication avec iNaturalist impossible\nVerifier la connexion internet",
-                            "en": "Failed communication with iNaturalist\nCheck internet connection"}[lang])],
-                  [sg.Ok()]]
-        window2 = sg.Window('SpeciesGuessr', layout, font=('Helvetica', 15), finalize=True)
-        while True:
-            event, values = window2.read()
-            if event in (sg.WIN_CLOSED, "Ok"):
+        if species_info.species_list is None:
+            end = True
+            layout = [[sg.Text({"fr": "Communication avec iNaturalist impossible\nVerifier la connexion internet",
+                                "en": "Failed communication with iNaturalist\nCheck internet connection"}[lang])],
+                      [sg.Ok()]]
+            window2 = sg.Window('SpeciesGuessr', layout, font=('Helvetica', 15), finalize=True, icon="logo.ico")
+            while True:
+                event, values = window2.read()
+                if event in (sg.WIN_CLOSED, "Ok"):
+                    break
+            window2.close()
+        else:
+            guessr = Guessr(species_info)
+            species_to_guess, image, attribution = guessr.get_new_guess(config, window)
+        window.close()
+
+    if mode in ["easy", "medium"] and not end:
+        layout = [[sg.Column([[sg.Text(f"{values['places']} - {values['taxons']} - {species_info.nb_species} {text_dict['species'][lang]}"),
+                               sg.Text(f"     Photo : {attribution}", key="attribution")]], justification="center")],
+                  [sg.Image(key="-IMAGE-", size=(config.width, config.height))],
+                  [sg.Column([[sg.Text("Score :"), sg.Text("0/0", key="-SV-"),
+                               sg.Text("  Accuracy :"), sg.Text("100%", key="-AV-")]],
+                             justification="center")],
+                  [sg.B(key='A1', expand_x=True), sg.B(key='A2', expand_x=True),
+                   sg.B(key='A3', expand_x=True), sg.B(key='A4', expand_x=True)]]
+
+        window = sg.Window('SpeciesGuessr', layout, location=(20, 20), finalize=True,
+                           return_keyboard_events=True, font=('Helvetica', 15), icon="logo.ico")
+        window["-IMAGE-"].update(data=ImageTk.PhotoImage(image))
+        if mode == "easy":
+            answers = set_random_answers(guessr, window, species_to_guess)
+        elif mode == "medium":
+            answers = set_neighbor_answers(guessr, window, species_to_guess)
+
+        window.TKroot.focus_force()
+
+        verify, success, fails = False, 0, 0
+        while not end:
+            event, values = window.read()
+            if event == sg.WIN_CLOSED or event == 'Escape:27':
+                mode = False
                 break
-        window2.close()
-    else:
-        guessr = Guessr(species_info)
-        species_to_guess, image, attribution = guessr.get_new_guess(config, window)
-    window.close()
-
-if mode in ["easy", "medium"] and not end:
-    layout = [[sg.Column([[sg.Text(f"{values['places']} - {values['taxons']} - {species_info.nb_species} {text_dict['species'][lang]}"),
-                           sg.Text(f"     Photo : {attribution}", key="attribution")]], justification="center")],
-              [sg.Image(key="-IMAGE-", size=(config.width, config.height))],
-              [sg.Column([[sg.Text("Score :"), sg.Text("0/0", key="-SV-"),
-                           sg.Text("  Accuracy :"), sg.Text("100%", key="-AV-")]],
-                         justification="center")],
-              [sg.B(key='A1', expand_x=True), sg.B(key='A2', expand_x=True),
-               sg.B(key='A3', expand_x=True), sg.B(key='A4', expand_x=True)]]
-
-    window = sg.Window('SpeciesGuessr', layout, location=(20, 20), finalize=True,
-                       return_keyboard_events=True, font=('Helvetica', 15))
-    window["-IMAGE-"].update(data=ImageTk.PhotoImage(image))
-    if mode == "easy":
-        answers = set_random_answers(guessr, window, species_to_guess)
-    elif mode == "medium":
-        answers = set_neighbor_answers(guessr, window, species_to_guess)
-
-    window.TKroot.focus_force()
-
-    verify, success, fails = False, 0, 0
-    while not end:
-        event, values = window.read()
-        print(event, type(event))
-        if event == sg.WIN_CLOSED or event == 'Escape:27':
-            break
-        if type(event) == str and (event[0] == "A"):
-            verify, i = True, int(event[1])
-        elif type(event) == str and event in ["1", "2", "3", "4"]:
-            verify, i = True, int(event[0])
-        if verify:
-            if species_to_guess == answers[i-1]:
-                fail = False
-                success += 1
-                window[f"A{i}"].update(button_color="green")
-            else:
-                fail = True
-                fails += 1
-                window[f"A{i}"].update(button_color="red")
-                j = answers.index(species_to_guess)
-                window[f"A{j+1}"].update(button_color="green")
-            window["-SV-"].update(f"{success}/{success+fails}")
-            window["-AV-"].update(f"{int(success/(success+fails)*100)}%")
-            window.refresh()
-            if fail:
-                sleep(1)
-
-            species_to_guess, image, attribution = guessr.get_new_guess(config)
-            window["-IMAGE-"].update(data=ImageTk.PhotoImage(image))
-            window["attribution"].update(f"     Photo : {attribution}")
-            if mode == "easy":
-                answers = set_random_answers(guessr, window, species_to_guess)
-            elif mode == "medium":
-                answers = set_neighbor_answers(guessr, window, species_to_guess)
-            verify = False
-    window.close()
+            if type(event) == str and (event[0] == "A"):
+                verify, i = True, int(event[1])
+            elif type(event) == str and event in ["1", "2", "3", "4"]:
+                verify, i = True, int(event[0])
+            if verify:
+                if species_to_guess == answers[i-1]:
+                    fail = False
+                    success += 1
+                    window[f"A{i}"].update(button_color="green")
+                else:
+                    fail = True
+                    fails += 1
+                    window[f"A{i}"].update(button_color="red")
+                    j = answers.index(species_to_guess)
+                    window[f"A{j+1}"].update(button_color="green")
+                window["-SV-"].update(f"{success}/{success+fails}")
+                window["-AV-"].update(f"{int(success/(success+fails)*100)}%")
+                window.refresh()
+                if fail:
+                    sleep(1)
+    
+                species_to_guess, image, attribution = guessr.get_new_guess(config)
+                window["-IMAGE-"].update(data=ImageTk.PhotoImage(image))
+                window["attribution"].update(f"     Photo : {attribution}")
+                if mode == "easy":
+                    answers = set_random_answers(guessr, window, species_to_guess)
+                elif mode == "medium":
+                    answers = set_neighbor_answers(guessr, window, species_to_guess)
+                verify = False
+        window.close()
