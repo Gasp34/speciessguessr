ebe9139dff4e50b7c46dba6c1112af3775a145fa
gaspard.dussert <gaspard.dussert@creatis.insa-lyon.fr>
2022-12-03 21:41:26 +0100

    better management of callbacks

--- speciesguessr/dash_gui2.py
+++ speciesguessr/dash_gui2.py
@@ -30,29 +30,36 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
-    html.Img(style={'height': '100%', 'width': '100%'}, id="image"),
+    html.Img(style={'height': '50%', 'width': '50%'}, id="image"),
     html.Button(id='B0', n_clicks=0, children='Submit'),
     html.Button(id='B1', n_clicks=0, children='Submit'),
     html.Button(id='B2', n_clicks=0, children='Submit'),
     html.Button(id='B3', n_clicks=0, children='Submit'),
+
+    dcc.Store(id='answers')
 ])
 
 
 @app.callback(Output('image', 'src'),
-              Output('B0', "children"),
-              Output('B1', "children"),
-              Output('B2', "children"),
-              Output('B3', "children"),
+              Output('answers', "data"),
               Input('B0', 'n_clicks'))
-def update_output(n_clicks):
+def update_image(n_clicks):
     species_to_guess = guessr.get_random_species()
     obs = guessr.find_obs_with_photo(species_to_guess["id"])
     photo = obs.photos[0]
     answers = get_random_answers(guessr, species_to_guess)
-    b = [answer["preferred_common_name"].capitalize() for answer in answers]
-    return photo.url_size("large"), b[0], b[1], b[2], b[3]
+    return photo.url_size("large"), answers
 
+
+@app.callback(Output('B0', "children"),
+              Output('B1', "children"),
+              Output('B2', "children"),
+              Output('B3', "children"),
+              Input("answers", "data"))
+def update_output(answers):
+    b = [answer["preferred_common_name"].capitalize() for answer in answers]
+    return b[0], b[1], b[2], b[3]
 
 if __name__ == '__main__':
-    # app.run_server(debug=True)
-    app.run_server(host="0.0.0.0", port="8050")
+    app.run_server(debug=True)
+    # app.run_server(host="0.0.0.0", port="8050")
